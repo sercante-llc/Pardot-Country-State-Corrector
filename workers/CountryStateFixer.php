3bf8e2099747366bad72d33a34040cf9cdc3b657
Mike Creuzer <github.com@creuzer.com>
2019-01-28 16:48:15 -0600

    Set up ENV variable logins
    Use the ENV variables so we can run it either locally or on Heroku easily

--- workers/CountryStateFixer.php
+++ workers/CountryStateFixer.php
@@ -1,6 +1,94 @@
 <?php
 
-echo "Hello World";
+
+// Pull the login out of the ENV as per the 12 Factor App https://12factor.net/ 
+$pardotLogin  = getenv('PardotLogin');
+$pardotPassword  = getenv('pardotPassword');
+$pardotUserKey  = getenv('pardotUserKey');
+
+
+
+//this will log in and print your API Key (good for 1 hour) to the console
+echo callPardotApi('https://pi.pardot.com/api/login/version/3',
+    array(
+        'email' => $pardotLogin,
+        'password' => $pardotPassword,
+        'user_key' => $pardotUserKey //available from https://pi.pardot.com/account
+    ),
+    'POST'
+);
+
+
+
+
+
+/**
+ * Call the Pardot API and get the raw XML response back
+ *
+ * @param string $url the full Pardot API URL to call, e.g. "https://pi.pardot.com/api/prospect/version/3/do/query"
+ * @param array $data the data to send to the API - make sure to include your api_key and user_key for authentication
+ * @param string $method the HTTP method, one of "GET", "POST", "DELETE"
+ * @return string the raw XML response from the Pardot API
+ * @throws Exception if we were unable to contact the Pardot API or something went wrong
+ */
+function callPardotApi($url, $data, $method = 'GET')
+{
+    // build out the full url, with the query string attached.
+    $queryString = http_build_query($data, null, '&');
+    if (strpos($url, '?') !== false) {
+        $url = $url . '&' . $queryString;
+    } else {
+        $url = $url . '?' . $queryString;
+    }
+
+    $curl_handle = curl_init($url);
+
+    // wait 5 seconds to connect to the Pardot API, and 30
+    // total seconds for everything to complete
+    curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 5);
+    curl_setopt($curl_handle, CURLOPT_TIMEOUT, 30);
+
+    // https only, please!
+    curl_setopt($curl_handle, CURLOPT_PROTOCOLS, CURLPROTO_HTTPS);
+
+    // ALWAYS verify SSL - this should NEVER be changed. 2 = strict verify
+    curl_setopt($curl_handle, CURLOPT_SSL_VERIFYHOST, 2);
+
+    // return the result from the server as the return value of curl_exec instead of echoing it
+    curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+
+    if (strcasecmp($method, 'POST') === 0) {
+        curl_setopt($curl_handle, CURLOPT_POST, true);
+    } elseif (strcasecmp($method, 'GET') !== 0) {
+        // perhaps a DELETE?
+        curl_setopt($curl_handle, CURLOPT_CUSTOMREQUEST, strtoupper($method));
+    }
+
+    $pardotApiResponse = curl_exec($curl_handle);
+    if ($pardotApiResponse === false) {
+        // failure - a timeout or other problem. depending on how you want to handle failures,
+        // you may want to modify this code. Some folks might throw an exception here. Some might
+        // log the error. May you want to return a value that signifies an error. The choice is yours!
+
+        // let's see what went wrong -- first look at curl
+        $humanReadableError = curl_error($curl_handle);
+
+        // you can also get the HTTP response code
+        $httpResponseCode = curl_getinfo($curl_handle, CURLINFO_HTTP_CODE);
+
+        // make sure to close your handle before you bug out!
+        curl_close($curl_handle);
+
+        throw new Exception("Unable to successfully complete Pardot API call to $url -- curl error: \"".
+                                "$humanReadableError\", HTTP response code was: $httpResponseCode");
+    }
+
+    // make sure to close your handle before you bug out!
+    curl_close($curl_handle);
+
+    return $pardotApiResponse;
+}
+
 
 
 ?>
