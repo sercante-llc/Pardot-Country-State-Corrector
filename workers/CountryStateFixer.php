3c9f49aa385ab16c30f269c1c3d26d633f858301
Mike Creuzer <github.com@creuzer.com>
2019-01-28 20:30:23 -0600

    Audit of Country and State records now available
    This now does an audit of country and state records by API. It doesn't yet push the changes back to Pardot.

--- workers/CountryStateFixer.php
+++ workers/CountryStateFixer.php
@@ -1,21 +1,21 @@
 <?php
 
-// state correction file
+// Load correction files
+// TODO make this load out of an ENV variable to provide the specific file and to opt into that specific function
+$StateCorrections = csv_to_array($filename='correction_options/states_ISOtoEnglish.csv');
+$CountryCorrections = csv_to_array($filename='correction_options/countries_ISOtoEnglish.csv');
 
-$csv = csv_to_array($filename='correction_options/states_ISOtoEnglish.csv');
-
-//print_r($csv);
 
 
 
 //Get the API Key from the server. This is good for 1 hour.
 $getAPIKey =  callPardotApi('https://pi.pardot.com/api/login/version/4',
-    array(
-        'email' => getenv('pardotLogin'),
-        'password' => getenv('pardotPassword'),
-        'user_key' => getenv('pardotUserKey') //available from https://pi.pardot.com/account
-    ),
-    'POST'
+	array(
+		'email' => trim(getenv('pardotLogin')),
+		'password' => trim(getenv('pardotPassword')),
+		'user_key' => trim(getenv('pardotUserKey')) //available from https://pi.pardot.com/account
+	),
+	'POST'
 );
 //print_r($getAPIKey);
 $APIKey = $getAPIKey['api_key'];
@@ -29,66 +29,78 @@ $APIKey = $getAPIKey['api_key'];
 // Lets look for recent Prospect record changes
 
 $results =  callPardotApi('https://pi.pardot.com/api/prospect/version/4/do/query?',
-    array(
-        'email' => getenv('pardotLogin'),
-        'password' => getenv('pardotPassword'),
-        'user_key' => getenv('pardotUserKey'), //available from https://pi.pardot.com/account
-	'api_key' => $APIKey, // requested from the server previously
-	//'last_activity_after' => '360 minutes ago',
-	'last_activity_after' => '1 days ago',
-	'fields' => 'email,country,state' // Optional list for speeding up the process by getting just the data we need.
-    ),
-    'POST'
+	array(
+		'email' => trim(getenv('pardotLogin')),
+		'password' => trim(getenv('pardotPassword')),
+		'user_key' => trim(getenv('pardotUserKey')), //available from https://pi.pardot.com/account
+		'api_key' => $APIKey, // requested from the server previously
+		'last_activity_after' => '21 minutes ago',
+		//'last_activity_after' => '1 days ago',
+		'fields' => 'email,country,state' // Optional list for speeding up the process by getting just the data we need.
+	),
+	'POST'
 );
-print_r($results);
+//print_r($results);
+loop_the_results($results);
+
+// Lets look for new Prospect record changes
+$results =  callPardotApi('https://pi.pardot.com/api/prospect/version/4/do/query?',
+	array(
+		'email' => trim(getenv('pardotLogin')),
+		'password' => trim(getenv('pardotPassword')),
+		'user_key' => trim(getenv('pardotUserKey')), //available from https://pi.pardot.com/account
+		'api_key' => $APIKey, // requested from the server previously
+		'created_after' => '21 minutes ago',
+		//'created_after' => '1 days ago',
+		'fields' => 'email,country,state' // Optional list for speeding up the process by getting just the data we need.
+	),
+	'POST'
+);
+//print_r($results);
+loop_the_results($results);
 
 
 
 
 
-
-
-
-
-
-
-// Lets process the data
-
-foreach($results['result'] as $key => $value)
+function loop_the_results($results)
 {
-	if($key == 'total_results')
-	{
-		if($value == 0 )
-		{
-			echo "No data to process\n";
-			break; // nothing to do here
-		}
-	}elseif($key == 'prospect')
-	{
-		if(isset($value['state']) && !empty($value['state']) && isset($csv[$value['state']]))
-		{
-			echo "Need to update state {$value['state']} to {$csv[$value['state']]} for {$value['email']}\n";
-		}elseif($value[0]['id']) // we are looking at an array of data, lets loop over it.
-		{
-			print_r($value);
-			foreach ($value AS $prospect)
-			{
-				if(isset($prospect['state']) && !empty($prospect['state']) && isset($csv[$prospect['state']]))
-				{
-					echo "Need to update state {$prospect['state']} to {$csv[$prospect['state']]} for {$prospect['email']}\n";
-				}
-			}
 
+	// Lets process the data
+
+	foreach($results['result'] as $key => $value)
+	{
+		if($key == 'total_results')
+		{
+			if($value == 0 )
+			{
+				//echo "No data to process\n";
+				break; // nothing to do here
+			}
+		}elseif($key == 'prospect')
+		{
+			if(isset($value['id']) ) // A single result, lets use it
+			{
+				search_for_errors($value);
+			}elseif($value[0]['id']) // we are looking at an array of data, lets loop over it.
+			{
+				//print_r($value);
+				foreach ($value AS $prospect)
+				{
+					search_for_errors($prospect);
+				}
+
+			}else
+			{
+				print_r($results);
+			}
 		}else
 		{
 			print_r($results);
 		}
-	}else
-	{
-		print_r($results);
+
+
 	}
-
-
 }
 
 
@@ -96,6 +108,26 @@ foreach($results['result'] as $key => $value)
 
 
 
+function search_for_errors($prospect)
+{
+	global $StateCorrections, $CountryCorrections; // Lets pull in some data sets
+	//print_r($prospect);
+
+	// Country error
+	//
+	if(isset($prospect['state']) && !empty($prospect['state']) && isset($StateCorrections[$prospect['state']]))
+	{
+		echo "Need to update state {$prospect['state']} to {$StateCorrections[$prospect['state']]} for {$prospect['email']}\n";
+	}
+
+	// State error
+	//
+	if(isset($prospect['country']) && !empty($prospect['country']) && isset($StateCorrections[$prospect['country']]))
+	{
+		echo "Need to update country {$prospect['country']} to {$CountryCorrections[$prospect['country']]} for {$prospect['email']}\n";
+	}
+
+}
 
 
 
@@ -110,6 +142,8 @@ foreach($results['result'] as $key => $value)
 
 
 /**
+ * Adapted from Pardot Reference API call http://developer.pardot.com/#sample-code 
+ *
  * Call the Pardot API and get the --raw-XML-- associative array response back
  *
  * @param string $url the full Pardot API URL to call, e.g. "https://pi.pardot.com/api/prospect/version/3/do/query"
@@ -120,93 +154,94 @@ foreach($results['result'] as $key => $value)
  */
 function callPardotApi($url, $data, $method = 'GET')
 {
-    // build out the full url, with the query string attached.
-    $queryString = http_build_query($data, null, '&');
-    if (strpos($url, '?') !== false) {
-        $url = $url . '&' . $queryString;
-    } else {
-        $url = $url . '?' . $queryString;
-    }
-//echo $url . "\n\n";
-    $curl_handle = curl_init($url);
+	// build out the full url, with the query string attached.
+	$queryString = http_build_query($data, null, '&');
+	if (strpos($url, '?') !== false) {
+		$url = $url . '&' . $queryString;
+	} else {
+		$url = $url . '?' . $queryString;
+	}
+	//echo $url . "\n\n";
+	$curl_handle = curl_init($url);
 
-    // wait 5 seconds to connect to the Pardot API, and 30
-    // total seconds for everything to complete
-    curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 5);
-    curl_setopt($curl_handle, CURLOPT_TIMEOUT, 30);
+	// wait 5 seconds to connect to the Pardot API, and 30
+	// total seconds for everything to complete
+	curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 5);
+	curl_setopt($curl_handle, CURLOPT_TIMEOUT, 30);
 
-    // https only, please!
-    curl_setopt($curl_handle, CURLOPT_PROTOCOLS, CURLPROTO_HTTPS);
+	// https only, please!
+	curl_setopt($curl_handle, CURLOPT_PROTOCOLS, CURLPROTO_HTTPS);
 
-    // ALWAYS verify SSL - this should NEVER be changed. 2 = strict verify
-    curl_setopt($curl_handle, CURLOPT_SSL_VERIFYHOST, 2);
+	// ALWAYS verify SSL - this should NEVER be changed. 2 = strict verify
+	curl_setopt($curl_handle, CURLOPT_SSL_VERIFYHOST, 2);
 
-    // return the result from the server as the return value of curl_exec instead of echoing it
-    curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+	// return the result from the server as the return value of curl_exec instead of echoing it
+	curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
 
-    if (strcasecmp($method, 'POST') === 0) {
-        curl_setopt($curl_handle, CURLOPT_POST, true);
-    } elseif (strcasecmp($method, 'GET') !== 0) {
-        // perhaps a DELETE?
-        curl_setopt($curl_handle, CURLOPT_CUSTOMREQUEST, strtoupper($method));
-    }
+	if (strcasecmp($method, 'POST') === 0) {
+		curl_setopt($curl_handle, CURLOPT_POST, true);
+	} elseif (strcasecmp($method, 'GET') !== 0) {
+		// perhaps a DELETE?
+		curl_setopt($curl_handle, CURLOPT_CUSTOMREQUEST, strtoupper($method));
+	}
 
-    $pardotApiResponse = curl_exec($curl_handle);
-    if ($pardotApiResponse === false) {
-        // failure - a timeout or other problem. depending on how you want to handle failures,
-        // you may want to modify this code. Some folks might throw an exception here. Some might
-        // log the error. May you want to return a value that signifies an error. The choice is yours!
+	$pardotApiResponse = curl_exec($curl_handle);
+	if ($pardotApiResponse === false) {
+		// failure - a timeout or other problem. depending on how you want to handle failures,
+		// you may want to modify this code. Some folks might throw an exception here. Some might
+		// log the error. May you want to return a value that signifies an error. The choice is yours!
 
-        // let's see what went wrong -- first look at curl
-        $humanReadableError = curl_error($curl_handle);
+		// let's see what went wrong -- first look at curl
+		$humanReadableError = curl_error($curl_handle);
 
-        // you can also get the HTTP response code
-        $httpResponseCode = curl_getinfo($curl_handle, CURLINFO_HTTP_CODE);
+		// you can also get the HTTP response code
+		$httpResponseCode = curl_getinfo($curl_handle, CURLINFO_HTTP_CODE);
 
-        // make sure to close your handle before you bug out!
-        curl_close($curl_handle);
+		// make sure to close your handle before you bug out!
+		curl_close($curl_handle);
 
-        throw new Exception("Unable to successfully complete Pardot API call to $url -- curl error: \"".
-                                "$humanReadableError\", HTTP response code was: $httpResponseCode");
-    }
+		throw new Exception("Unable to successfully complete Pardot API call to $url -- curl error: \"".
+			"$humanReadableError\", HTTP response code was: $httpResponseCode");
+	}
 
-    // make sure to close your handle before you bug out!
-    curl_close($curl_handle);
+	// make sure to close your handle before you bug out!
+	curl_close($curl_handle);
 
 
-    // Quick and dirty way of XML -> array
+	// Quick and dirty way of XML -> array
 
-    $xml = simplexml_load_string($pardotApiResponse);
-    $json  = json_encode($xml);
-    $pardotApiResponseData = json_decode($json, true);
+	$xml = simplexml_load_string($pardotApiResponse);
+	$json  = json_encode($xml);
+	$pardotApiResponseData = json_decode($json, true);
 
-    if($pardotApiResponseData['stat'] = 'fail')
-    {
-	    echo $url . "\n\n" . $pardotApiResponse;
-	    exit();
-    }
+	if(isset($pardotApiResponseData['@attributes']['stat']) && $pardotApiResponseData['@attributes']['stat'] == 'fail')
+	{
+		echo $url . "\n\n" . $pardotApiResponse;
+		print_r($pardotApiResponseData);
+		exit();
+	}
 
-    return $pardotApiResponseData;
+	return $pardotApiResponseData;
 }
 
 
 
 function csv_to_array($filename='', $delimiter=',')
 {
-    if(!file_exists($filename) || !is_readable($filename))
-        return FALSE;
+	if(!file_exists($filename) || !is_readable($filename))
+		return FALSE;
 
-    $header = NULL;
-    $data = array();
-    if (($handle = fopen($filename, 'r')) !== FALSE)
-    {
-        while (($row = fgetcsv($handle, 1000, $delimiter)) !== FALSE)
-	{ 
-                $data[trim($row[0])] = trim($row[1]);
-        }
-        fclose($handle);
-    }
-    return $data;
+	$header = NULL;
+	$data = array();
+	if (($handle = fopen($filename, 'r')) !== FALSE)
+	{
+		while (($row = fgetcsv($handle, 1000, $delimiter)) !== FALSE)
+		{ 
+			$data[trim($row[0])] = trim($row[1]);
+		}
+		fclose($handle);
+	}
+	return $data;
 }
 
 ?>
